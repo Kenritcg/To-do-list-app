6b85cee9d3743cda41897e0e39253878f8ad961f
Kenritcg <158083607+Kenritcg@users.noreply.github.com>
2026-01-16 09:27:43 +0100

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart ';
+import 'package:daily_planner/Screens/task_page.dart';
+
+void main() async {
+  await Hive.initFlutter("hive_box");
+  runApp(const Todoapp());
+}
+
+class Todoapp extends StatelessWidget {
+  const Todoapp({super.key});
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: TaskPage(),
+    );
+  }
+}
